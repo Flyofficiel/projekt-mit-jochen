ce2918ab3e4141c24a5f37e0ca34a25130e2410e
lucas Huber <lucashuber667@gmail.com>
2024-09-09 08:41:42 +0200

    .

--- obj/Debug/net8.0/lucas.AssemblyInfo.cs
+++ obj/Debug/net8.0/lucas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lucas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa871e1b15f0ba0f5ced97aa6b0073236dd7ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2e09b250db6d71b27cae4f8a88738474d39a8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("lucas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lucas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
